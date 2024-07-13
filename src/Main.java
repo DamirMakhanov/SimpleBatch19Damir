7e25e706c80d57a72cb3a86e6908a85ead2e52a7
makhanov <makhanov24@gmail.com>
2024-07-12 22:08:18 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Batch 19 is great!");
+            Thread.sleep(2000);
+
+        }
     }
 }
